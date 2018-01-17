177b915fe933e41841ef8c4c4aa80a4c74f38662
admin <jordi.montana@gmail.com>
2018-01-17 09:10:28 +0100

    Added first failed unit test (to pass)

--- D365Saturday.Plugins.Tests/UnitTest1.cs
+++ D365Saturday.Plugins.Tests/UnitTest1.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿using FakeXrmEasy;
+using System;
+using TypedEntities;
 using Xunit;
+using System.Linq;
 
 namespace D365Saturday.Plugins.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void TestMethod1()
+        public void Should_create_phone_call_history_on_create_of_a_phonecall()
         {
+            var ctx = new XrmFakedContext();
+            var plugCtx = ctx.GetDefaultPluginContext();
+
+            ctx.ExecutePluginWith<PhoneCallCreatePlugin>(plugCtx);
+
+            var historyRecords = ctx.CreateQuery<ultra_phonecallhistory>().ToList();
+            Assert.Equal(1, historyRecords.Count);
         }
     }
 }
---
+++ D365Saturday.Plugins/PhoneCallCreatePlugin.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D365Saturday.Plugins
+{
+    public class PhoneCallCreatePlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            #region Boilerplate
+            // Obtain the execution context from the service provider.
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // Obtain the organization service reference.
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+            #endregion
+
+            throw new NotImplementedException();
+        }
+    }
+}
