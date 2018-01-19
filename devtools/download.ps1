b279990ee0ffbc7bcc9b0a4ae6982b80fc91244f
admin <jordi.montana@gmail.com>
2018-01-20 00:28:53 +0100

    Added developer tools script. Signed codeactivities.

---
+++ devtools/download.ps1
@@ -0,0 +1,46 @@
+$sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+$targetNugetExe = ".\nuget.exe"
+Remove-Item .\Tools -Force -Recurse -ErrorAction Ignore
+Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+Set-Alias nuget $targetNugetExe -Scope Global -Verbose
+
+##
+##Download Plugin Registration Tool
+##
+./nuget install Microsoft.CrmSdk.XrmTooling.PluginRegistrationTool -O .\Tools
+md .\Tools\PluginRegistration
+$prtFolder = Get-ChildItem ./Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.PluginRegistrationTool.'}
+move .\Tools\$prtFolder\tools\*.* .\Tools\PluginRegistration
+Remove-Item .\Tools\$prtFolder -Force -Recurse
+
+##
+##Download CoreTools
+##
+./nuget install  Microsoft.CrmSdk.CoreTools -O .\Tools
+md .\Tools\CoreTools
+$coreToolsFolder = Get-ChildItem ./Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.CoreTools.'}
+move .\Tools\$coreToolsFolder\content\bin\coretools\*.* .\Tools\CoreTools
+Remove-Item .\Tools\$coreToolsFolder -Force -Recurse
+
+##
+##Download Configuration Migration
+##
+./nuget install  Microsoft.CrmSdk.XrmTooling.ConfigurationMigration.Wpf -O .\Tools
+md .\Tools\ConfigurationMigration
+$configMigFolder = Get-ChildItem ./Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.ConfigurationMigration.Wpf.'}
+move .\Tools\$configMigFolder\tools\*.* .\Tools\ConfigurationMigration
+Remove-Item .\Tools\$configMigFolder -Force -Recurse
+
+##
+##Download Package Deployer 
+##
+./nuget install  Microsoft.CrmSdk.XrmTooling.PackageDeployment.WPF -O .\Tools
+md .\Tools\PackageDeployment
+$pdFolder = Get-ChildItem ./Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.PackageDeployment.Wpf.'}
+move .\Tools\$pdFolder\tools\*.* .\Tools\PackageDeployment
+Remove-Item .\Tools\$pdFolder -Force -Recurse
+
+##
+##Remove NuGet.exe
+##
+Remove-Item nuget.exe    
