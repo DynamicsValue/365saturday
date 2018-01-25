69c2fec3568efbab5531cf7c1cf0c3d7adfc4a00
admin <jordi.montana@gmail.com>
2018-01-25 18:55:32 +0100

    Updated fakexrmeasy... again

--- D365Saturday.WebResources/test/WebApiHelperTests.js
+++ D365Saturday.WebResources/test/WebApiHelperTests.js
@@ -1,22 +1,24 @@
 
 
-var PhoneCallHistoryUtils = require('../src/app/utils/PhoneCallHistoryUtils');
+var PhoneCallHistoryUtils = require('../src/app/utils/PhoneCallHistoryUtils.js');
+
 var assert = require('chai').assert;
 var Guid = require('guid');
 var egde = require('edge');
 
 describe("WebApiHelper Tests", function () {
 
-    var xrmFakedContext = require('fakexrmeasy.v9');
+    var xrmFakedContext = require('fakexrmeasy');
     xrmFakedContext.setProxyPath('../packages/FakeXrmEasy.EdgeProxy.v9.0.0.1/lib/net452', 'v9');
     
     it("Should_retrieve_phonecall_history_records_for_a_phonenumber", function(done) {
 
         //Arrange
+        
         var contact1 = {id: Guid.create(), logicalName: "contact"};
         var contact2 = {id: Guid.create(), logicalName: "contact"};
-        var phoneHistory1 = { id: Guid.create(), logicalName: "ultra_phonecallhistory", ultra_phonenumber: '+44666 666 666', ultra_contactid = contact1 };
-        var phoneHistory2 = { id: Guid.create(), logicalName: "ultra_phonecallhistory", ultra_phonenumber: '+44777 666 666', ultra_contactid = contact2 };
+        var phoneHistory1 = { id: Guid.create(), logicalName: "ultra_phonecallhistory", ultra_phonenumber: '+44666 666 666', ultra_contactid: contact1 };
+        var phoneHistory2 = { id: Guid.create(), logicalName: "ultra_phonecallhistory", ultra_phonenumber: '+44777 666 666', ultra_contactid: contact2 };
         
         xrmFakedContext.initialize([
             contact1,
