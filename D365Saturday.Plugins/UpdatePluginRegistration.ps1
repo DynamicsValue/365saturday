2e3f745935ae067bde7d5267d5456ed582aeed9b
Wael Hamze <contact@waelhamze.com>
2018-03-28 21:14:32 +0100

    Added Scripts to Update Plugin Steps

---
+++ D365Saturday.Plugins/UpdatePluginRegistration.ps1
@@ -0,0 +1,24 @@
+﻿[CmdletBinding()]
+
+param
+(
+	[string]$CrmConnectionString #The connection string as per CRM Sdk
+)
+
+$ErrorActionPreference = "Stop"
+
+#Script Location
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+Write-Verbose "Script Path: $scriptPath"
+
+Write-Verbose "ConnectionString = $connectionString"
+
+if ($CrmConnectionString -eq '')
+{
+	$CrmConnectionString = $Env:CrmCon
+}
+$AssemblyPath = "$scriptPath\bin\Debug\D365Saturday.Plugins.dll"
+$SolutionName = 'Customizations'
+$MappingJsonPath = "$scriptPath\PluginRegistrationMapping.json"
+
+& "$scriptPath\..\devtools\Tools\xRMCIFramework\PluginRegistration.ps1" -Verbose -CrmConnectionString "$CrmConnectionString" -AssemblyPath "$AssemblyPath" -MappingJsonPath "$MappingJsonPath" -solutionName "Customizations"
